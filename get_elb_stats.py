ed64b2fdaac8994534d3762a1900e09ed6ecdb43
EC2 Default User <ec2-user@zabbix-i-c26791bc.vpc.us-east-1b.qa2.cloudhub.io>
2013-05-09 15:53:40 +0000

    Added more keys to JSON output

--- get_elb_stats.py
+++ get_elb_stats.py
@@ -18,43 +18,56 @@ from dns import resolver
 #       use boto to get health checks from ELBs in discovery
 #       catch boto exceptions
 #       extend for different regions
-
 def discover_elbs():
     regex = re.compile(".*\.elb\.amazonaws\.com")
-    elbs = []
-    c = boto.connect_route53()
-    zone = c.get_hosted_zone_by_name(options.zone)
+    data = [] # list to store the result
+    conn_elb = boto.connect_elb() # used to get ELB specific data
+    elbs = conn_elb.get_all_load_balancers() # get all ELBs
+    conn_r53 = boto.connect_route53() # used to get Route53 records
+    zone = conn_r53.get_hosted_zone_by_name(options.zone)
+    # FIX
     if not zone:
         sys.exit("ZBX_NOTSUPPORTED")
     # get clean zone_id, due to https://github.com/boto/boto/issues/448
     id = zone['GetHostedZoneResponse']['HostedZone']['Id'].replace('/hostedzone/','')
     # get all resource records
-    result = c.get_all_rrsets(id)
-    for item in result:
-        if item.type == "CNAME":
-            # check if matches *.elb.amazonaws.com
-            for record in item.resource_records:
-                if regex.match(record):
-                    # store results as dict
-                    elbs.append({'{#ELB_NAME}' : record})
+    result = conn_r53.get_all_rrsets(id)
+    try:
+        for item in result:
+            if item.type == "CNAME":
+                # check if matches *.elb.amazonaws.com
+                for record in item.resource_records:
+                    if regex.match(record):
+                        # search ELB by its dns_name property
+                        lb = (lb for lb in elbs if lb.dns_name==record).next()
+                        for listener in lb.listeners:
+                            # store results as dict
+                            data.append({'{#ELB_NAME}' : lb.name + "-" +
+                                        str(listener.load_balancer_port),
+                                        '{#ELB_DNS}' : lb.dns_name,
+                                        '{#ELB_PORT}' : listener.load_balancer_port,
+                                        '{#ELB_PROTO}' : listener.protocol,
+                                        '{#ELB_CHECK}' : lb.health_check.target})
+    except StopIteration:
+        pass
     # build JSON for Zabbix
     # https://www.zabbix.com/documentation/2.0/manual/discovery/low_level_discovery
-    if len(elbs) > 0:
-        discovery = {'data' : elbs}
-        print json.dumps(discovery)
+    discovery = {'data' : data}
+    print json.dumps(discovery)
 
 def check_elb():
+    pass
     # get just the name of the ELB
-    elb_name = string.split(options.check, ".")[0]
-    # get ELB configuration and extract its healthcheck
-    c = boto.connect_elb()
-    result = c.get_all_load_balancers(elb_name)
-    for lb in result:
-        print lb.dns_name
-    # get list of A records from the ELB
-    ips = resolver.query(options.check)
-    for ip in ips:
-        print ip
+    #elb_name = string.split(options.check, ".")[0]
+    ## get ELB configuration and extract its healthcheck
+    #c = boto.connect_elb()
+    #result = c.get_all_load_balancers(elb_name)
+    #for lb in result:
+    #    print lb.dns_name
+    ## get list of A records from the ELB
+    #ips = resolver.query(options.check)
+    #for ip in ips:
+    #    print ip
 
 def main(argv):
     # Help + option parsing
